8aa1c57fa67f46ec74c739d429cdf15dcfc2062a
oratake <smith.shimomura@gmail.com>
2024-09-24 00:32:38 +0900

    feat: クエリ、パラメータの取得例

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,12 @@ app.use("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+app.get("/posts/:id", (c) => {
+  const page = c.req.query("page");
+  const id = c.req.param("id");
+  c.header("X-Message", "Hi!");
+  return c.text(`You want see ${page} of ${id}`);
+});
 
 // API
 app.get("/api/hello", (c) => {
