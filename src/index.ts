c177928540ae37e0e0db620d1c64b8c7d5b78e59
oratake <smith.shimomura@gmail.com>
2024-09-23 23:47:47 +0900

    feat: fallbackなど基本的なルーティング追加

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
+app.use("/static/*", serveStatic({ root: "./" }));
+app.use("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+app.get("*", serveStatic({ path: "./static/fallback.txt" }));
 
 export default {
   port: 3000,
