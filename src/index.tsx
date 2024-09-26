22294f1e92b9358c2c198a1cbbdb0adfd4d87a7f
oratake <smith.shimomura@gmail.com>
2024-09-26 20:25:28 +0900

    feat: hono/jsxのjsxを使用

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,25 @@
 import { Hono } from "hono";
+import type { FC } from "hono/jsx";
 import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
 app.use("/static/*", serveStatic({ root: "./" }));
 app.use("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
+
+const View: FC = () => {
+  return (
+    <html>
+      <body>
+        <h1>Hello Hono!</h1>
+      </body>
+    </html>
+  );
+};
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.html(<View />);
 });
+
 app.get("/posts/:id", (c) => {
   const page = c.req.query("page");
   const id = c.req.param("id");
